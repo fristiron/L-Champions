af49bc89a481e79bc62a8594aefb0436ca45fec1
alrik94 <olsonalrik@gmail.com>
2014-10-21 16:39:31 -0700

    init

---
+++ L-Champions/L-Champions/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace L_Champions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
